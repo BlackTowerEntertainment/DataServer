66ee33f6a89b529ac6c49e69e618e247cd512bf9
Chris Watts <cwatts@simplymeasured.com>
2015-01-10 22:13:16 -0800

    Adding front end depedencies to bower like bootstrap

--- Brocfile.js
+++ Brocfile.js
@@ -16,5 +16,26 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+// Bootstrap CSS
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {destDir: 'assets'});
+// Bootstrap Fonts
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {destDir: 'fonts'});
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {destDir: 'fonts'});
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {destDir: 'fonts'});
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {destDir: 'fonts'});
+// Bootstrap JS
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+// Moment
+app.import('bower_components/moment/moment.js');
+// MD5 checksum
+app.import('bower_components/JavaScript-MD5/js/md5.js');
+//Font awesome CSS
+app.import('bower_components/font-awesome/css/font-awesome.css');
+//Font awesome Fonts
+app.import('bower_components/font-awesome/fonts/fontawesome-webfont.eot', {destDir: 'fonts'});
+app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf', {destDir: 'fonts'});
+app.import('bower_components/font-awesome/fonts/fontawesome-webfont.svg', {destDir: 'fonts'});
+app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', {destDir: 'fonts'});
 
 module.exports = app.toTree();
